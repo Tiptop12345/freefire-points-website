19f80ea1f8a45eff2523d5c42d67656003742ca7
Tiptop12345 <mdamirulnaimul54750@gmail.com>
2025-02-05 18:44:37 +0600

    Configured GitHub Pages deployment

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';  // Make sure React is imported
+import './App.css';          // Import your CSS file
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your JSX code here */}
+      <h1>Welcome to my Free Fire Points Website</h1>
+      {/* Add other components or JSX elements as needed */}
+    </div>
+  );
+}
+
+export default App;  // Export the App component for use in other parts of your app
